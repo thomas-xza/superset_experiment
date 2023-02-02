6470f22243dfd3e7bfd44e983272241a253ad245
thomas <thomas@xza.fr>
2023-02-02 19:15:53 +0000

    Finished with plotting most dangerous crimes to map

--- import_csv_police_data_outcomes.sql
+++ import_csv_police_data_outcomes.sql
@@ -1,4 +1,9 @@
 COPY crime_data_gmp_outcomes(crime_id, month, reported_by, falls_within, longitude, latitude, location, lsoa_c, lsoa_n, outcome_type)
-FROM '/tmp/2018_01_to_2019_06_gmp_police_outcomes_clean.csv'
+FROM '/tmp/2016_07_to_2017_12_gmp_police_outcomes_clean.csv'
 DELIMITER ','
 CSV HEADER;
+
+/*
+2018_01_to_2019_06_gmp_police_outcomes_clean.csv
+2016_07_to_2017_12_gmp_police_outcomes_clean.csv
+*/
