dd11d07b44bd0968d0a3cff93bc402c755b4b169
thomas <thomas@xza.fr>
2023-02-06 19:36:39 +0000

    Dangling commits

--- worldbank_data/cleanup_data.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-sed -i "/\.\..*$/d" processed_data/cost_of_healthy_diet_per_country_2020.csv
--- worldbank_data/create_table_worldbank_food_country.sql
+++
@@ -1,8 +0,0 @@
-	
-CREATE TABLE worldbank_food_cost_country (
-    id SERIAL,
-    country VARCHAR(64),
-    country_code VARCHAR(4),
-    cost DECIMAL(5,4),
-    PRIMARY KEY (id)
-);
